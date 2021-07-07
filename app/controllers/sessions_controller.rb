6814b1784bd88ccfe2edefb811281067e24927a6
Anthony <anthonykmbanks@gmail.com>
2021-07-07 14:42:47 -0400

    commit tree 5b30c8d75a92aa2696ca06502669dc06741fb5ec

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+    skip_before_action :authorize, only: :create
+
+    def create
+        user = find_user
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+    private
+    
+    def find_user
+        User.find_by(username: params[:username])
+    end
+
+end
